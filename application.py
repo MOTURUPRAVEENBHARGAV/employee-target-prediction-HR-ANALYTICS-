ed07b4215438abeb99b04d439f6469aa93e74b26
moturu praveen bhargav <56902823+MOTURUPRAVEENBHARGAV@users.noreply.github.com>
2023-02-14 23:11:01 +0530

    Update application.py

--- application.py
+++ application.py
@@ -190,6 +190,6 @@ def jsondata():
         
                 
 if __name__=="__main__":
-    app.run(debug=True)
+  app.run(debug=True)
 # if __name__=="__main__":
 #     app.run(host='0.0.0.0', port=8080)
